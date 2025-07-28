de226947a28826d2b7a385b2ca9a0f3a85ad57fc
Matteo Polito <mapo80@gmail.com>
2025-07-28 10:30:33 +0200

    Add extra .NET SDK tests

--- NoisePrintSdk.Tests/RunnerTests.cs
+++ NoisePrintSdk.Tests/RunnerTests.cs
@@ -13,4 +13,22 @@ public class RunnerTests
         Assert.Equal(4, shape.Length);
         Assert.True(time > 0);
     }
+
+    [Fact]
+    public void Run_JpegKnownQuality()
+    {
+        string img = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../")), "examples/faceswap.jpeg");
+        var (qf, shape, _) = OnnxRunner.Run(img);
+        Assert.Equal(95, qf);
+        Assert.Equal(4, shape.Length);
+    }
+
+    [Fact]
+    public void Run_PngReturns101()
+    {
+        string img = Path.Combine(Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../")), "examples/splicing.png");
+        var (qf, shape, _) = OnnxRunner.Run(img);
+        Assert.Equal(101, qf);
+        Assert.Equal(4, shape.Length);
+    }
 }
